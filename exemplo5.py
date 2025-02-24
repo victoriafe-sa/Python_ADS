7992a8dd43c61eecb2e65b7a94b78519d8277c7f
Victoria Ferreira <victoria.senac.13@gmail.com>
2025-02-24 11:41:16 -0300

    Create exemplo5.py

---
+++ exemplo5.py
@@ -0,0 +1,27 @@
+#Lista Simples[]
+lista = ['Senac', 'curso', 'Tecnologia', 'ADS', 2025, 3.25]
+print(lista)
+print(type(lista))
+print(lista[2])
+print('-'*30,'\n')
+
+#Tupla()
+tupla = ('Senac', 'curso', 'Tecnologia', 'ADS', True, 3.25)
+print(tupla)
+print(type(tupla))
+print(tupla[4])
+print('-'*30,'\n')
+
+#Dicionario
+dicionario = {"nome":"Ana", "Logica":False, "id":2,"Peso":78.5}
+print(dicionario)
+print(type(dicionario))
+print(dicionario['nome'])
+print('-'*30,'\n')
+
+#Conjunto
+conjunto = {'nome', True, 3, 3, 4.5}
+print(conjunto)
+print(type(conjunto))
+
+print('-'*30,'\n')
