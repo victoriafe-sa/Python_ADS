406b47573056ee2ebc0c5d4d751b52e00bddfa59
victoriafe-sa <victoria.senac.13@gmail.com>
2025-03-24 11:08:06 -0300

    Update funcao1.py

--- funcao1.py
+++ funcao1.py
@@ -5,15 +5,27 @@ def msg():
 
 msg()
 
-def soma(n1,n2):
+def s(n1,n2):
     #sum = n1+n2
     #print(f"A soma dos valores é: {sum}")
-    return n1+n2
-#soma(1,2)
-resultado = soma(5,6)
+   return n1+n2
+#s(1,2)
+resultado = s(5,6)
 print(f"A soma dos valores é: {resultado}")
+print('-'*30)
 
-def teste(x,y):
-    return x+y
-resultado = teste(int(input('Digite o primeiro valor: ')),int(input('Digite o segundo valor: ')))
-print(f"A soma dos valores é: {resultado}")
+def operacoes(x,y):
+    soma = x+y
+    sub = x-y
+    mult = x*y
+    div = x/y
+    #return soma, sub, mult, div
+    print(f"A soma dos valores é: {soma}")
+    print(f"A soma dos valores é: {sub}")
+    print(f"A soma dos valores é: {mult}")
+    print(f"A soma dos valores é: {div}")
+#soma, sub, mult, div = operacoes(int(input('Digite o primeiro valor: ')),int(input('Digite o segundo valor: ')))
+operacoes(int(input('Digite o primeiro valor: ')),int(input('Digite o segundo valor: ')))
+
+print('-'*30)
+
